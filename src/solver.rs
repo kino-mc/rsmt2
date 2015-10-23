ebca956f35b7adb13102fd5ec0b79ead4f370f4d
Adrien Champion <adrien.champion@mail.com>
2015-10-23 10:37:50 -0500

    license stuff

--- src/solver.rs
+++ src/solver.rs
@@ -1,6 +1,15 @@
-#![ doc = "
+// Copyright 2015 Adrien Champion. See the COPYRIGHT file at the top-level
+// directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+/*!
 Implements all the printing and parsing methods for the SMT lib 2 standard.
-"]
+*/
 
 
 use std::process::{ Command, Stdio } ;
