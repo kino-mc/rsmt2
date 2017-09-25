49d141df43654afb001510179e89b82de61bc4f8
Adrien Champion <adrien.champion@email.com>
2017-09-25 21:40:32 +0900

    rmed license text in sources, trying coveralls

--- src/conf.rs
+++ src/conf.rs
@@ -1,11 +1,3 @@
-// See the LICENSE files at the top-level directory of this distribution.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 //! Solver configuration, contains backend solver specific info.
 
 use std::fmt ;
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-// See the LICENSE files at the top-level directory of this distribution.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 #![doc = r#"A wrapper around an SMT Lib 2(.5)-compliant SMT solver.
 
 See [`CHANGES.md`](https://github.com/kino-mc/rsmt2/blob/master/README.md) for
